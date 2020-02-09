1fd489d227febc0e913cd0f59e0c31b590be8782
piotrek6598 <60852339+piotrek6598@users.noreply.github.com>
2020-02-09 22:35:05 +0100

    Update WatchDir.java

--- Lucyna/src/main/java/indekser/WatchDir.java
+++ Lucyna/src/main/java/indekser/WatchDir.java
@@ -189,27 +189,4 @@ public class WatchDir {
             
         }
     }
-
-    /*static void usage() {
-        System.err.println("usage: java WatchDir [-r] dir");
-        System.exit(-1);
-    }
-
-    public static void main(String[] args) throws IOException {
-        // parse arguments
-        if (args.length == 0 || args.length > 2)
-            usage();
-        boolean recursive = false;
-        int dirArg = 0;
-        if (args[0].equals("-r")) {
-            if (args.length < 2)
-                usage();
-            recursive = true;
-            dirArg++;
-        }
-
-        // register directory and process its events
-        Path dir = Paths.get(args[dirArg]);
-        new WatchDir(dir, recursive).processEvents();
-    }*/
 }
